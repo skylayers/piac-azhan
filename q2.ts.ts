53a864b474401479426bf3ad9c92856da45bb3b4
skylayers <150579629+skylayers@users.noreply.github.com>
2023-12-29 07:51:07 -0800

    Add files via upload

---
+++ q2.ts.ts
@@ -0,0 +1,2 @@
+let personal_message:string='Hello Nazir Would you like to learn some python today?';
+console.log(`${personal_message}`)
---
+++ q3.ts
@@ -0,0 +1,7 @@
+let f_name:string='Nazir ali baba  Rizwan';
+//1st 
+console.log(f_name.toLowerCase())
+//2nd
+console.log(f_name.toUpperCase())
+
+
